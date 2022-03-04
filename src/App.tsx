d6ea75db953fe76c42140aa6f56612fe73a3e3d8
Emma McCracken <emma.mccracken14@gmail.com>
2022-03-04 11:45:37 +0000

    add minimal styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [initialPlayers, setInitialPlayers] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>C3 Local Party</h1>
       </header>
     </div>
   );
